0a798b9880d43808fc0822075cd653ea5abbd6c5
Sophie Mann <sklm@live.co.uk>
2020-06-02 22:55:32 +0100

    Reorganise

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { ApolloServer } from "apollo-server-express";
 import { ApolloEngine } from "apollo-engine";
-import { typeDefs } from "./data/schema";
-import { resolvers } from "./data/resolvers";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
 
 dotenv.config();
 
@@ -14,29 +14,30 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  tracing: true,
-  cacheControl: {
-    defaultMaxAge: 5,
-    stripFormattedExtensions: false,
-    calculateCacheControlHeaders: false,
-  },
-  engine: false,
+  // tracing: true,
+  // cacheControl: {
+  //   defaultMaxAge: 5,
+  //   stripFormattedExtensions: false,
+  //   calculateCacheControlHeaders: false,
+  // },
+  // engine: false,
 });
+
 server.applyMiddleware({ app });
 
 const engine = new ApolloEngine({
   apiKey: process.env.ENGINE_API_KEY,
 });
 
-engine.listen(
-  {
-    port: GRAPHQL_PORT,
-    graphqlPaths: ["/api/graphql"],
-    expressApp: app,
-    launcherOptions: {
-      startupTimeout: 3000,
-    },
-  },
+app.listen(
+  // {
+  //   port: GRAPHQL_PORT,
+  //   graphqlPaths: ["/api/graphql"],
+  //   expressApp: app,
+  //   launcherOptions: {
+  //     startupTimeout: 3000,
+  //   },
+  // },
   () =>
     console.log(
       `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphql`
